10a482f351089b64e794c0cd33ebce84361b61a5
Shouvik Mukherjee <59367817+SHOUVIK-13@users.noreply.github.com>
2023-01-03 09:28:46 +0000

    2nd commit

--- Vi-CHAT BACKEND/controllers/auth/postRegister.js
+++ Vi-CHAT BACKEND/controllers/auth/postRegister.js
@@ -5,7 +5,7 @@ const postRegister = async (req,res) => {
         const { username, mail, password } = req.body;
 
         //check if user exists
-        const userExists = await User.exists({ mail });
+        const userExists = await User.exists({ mail: mail.toLowerCase() });
 
         if (userExists) {
             return res.status(409).send("Email is already present.");
@@ -16,6 +16,23 @@ const postRegister = async (req,res) => {
         const encryptPassword = await bcrypt.hash(password, 10);
 
         //create user and save in db
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptPassword,
+        });
+
+
+        // create JWT Token
+        const token = 'JWT TOKEN';
+
+        res.status(201).json({
+            userDetails: {
+                mail: user.mail,
+                token: token,
+                username: user.username,
+            }
+        });
         
 
     } catch(err){
